f672396c38485d6563a810752482384713f5a0d0
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-02-18 23:44:12 -0300

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 // Import stylesheets
+import { Person } from './person';
 import './style.css';
+
+let pessoa = new Person('Mario', 'Rocha', 'da Silva', 40);
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1> TypeScript's World </h1>
+<div>${pessoa.getFullName()}</div>
+<div>${pessoa.getBirthdayYear()}</div>
+`;
